8bf44085ffedc99fa6bb4bd9f7e5b844763ec129
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-10-02 15:58:20 +0200

    Tests for basic scenarios.

--- test/WebCreatorTest.cpp
+++ test/WebCreatorTest.cpp
@@ -1,3 +1,23 @@
 #include "gtest/gtest.h"
 
-TEST(WebCreator, test) { EXPECT_EQ(true, true); }
+#include "../WebCreator.h"
+
+TEST(WebCreator, checkConnectionEmptyWeb)
+{
+    WebCreator webCreator;
+    EXPECT_EQ(webCreator.connectionExist("1.1.1.1.1", "2.2.2.2.2"), false);
+}
+
+TEST(WebCreator, checkConnectionOneElementWeb)
+{
+    WebCreator webCreator;
+    webCreator.addConnection("1.1.1.1.1", "2.2.2.2.2");
+    EXPECT_EQ(webCreator.connectionExist("1.1.1.1.1", "2.2.2.2.2"), true);
+}
+
+TEST(WebCreator, checkConnectionOneElementWebReversed)
+{
+    WebCreator webCreator;
+    webCreator.addConnection("2.2.2.2.2", "1.1.1.1.1");
+    EXPECT_EQ(webCreator.connectionExist("1.1.1.1.1", "2.2.2.2.2"), true);
+}
