4833b77dd6c91656e771fa73c8a8696424a1c3d2
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-10-02 16:30:23 +0200

    Added test for creation of non trivial web.

--- test/WebCreatorTest.cpp
+++ test/WebCreatorTest.cpp
@@ -21,3 +21,20 @@ TEST(WebCreator, checkConnectionOneElementWebReversed)
     webCreator.addConnection("2.2.2.2.2", "1.1.1.1.1");
     EXPECT_EQ(webCreator.connectionExist("1.1.1.1.1", "2.2.2.2.2"), true);
 }
+
+TEST(WebCreator, testCreationOfNonTrivialWeb)
+{
+    WebCreator creator;
+    creator.addConnection("100.100.100.1", "100.100.100.2");
+    creator.addConnection("100.100.100.1", "100.100.100.3");
+    creator.addConnection("100.100.100.10", "100.100.100.11");
+    EXPECT_EQ(creator.connectionExist("100.100.100.1", "100.100.100.3"), true);
+    EXPECT_EQ(creator.connectionExist("100.100.100.10", "100.100.100.2"),
+              false);
+    EXPECT_EQ(creator.connectionExist("100.100.100.10", "100.100.100.11"),
+              true);
+    creator.addConnection("100.100.100.11", "100.100.100.2");
+    EXPECT_EQ(creator.connectionExist("100.100.100.10", "100.100.100.3"), true);
+    EXPECT_EQ(creator.connectionExist("100.100.100.100", "100.100.100.103"),
+              false);
+}
