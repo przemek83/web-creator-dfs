c5d3cad2fe8443d577397c4c5fb106a14e0f4768
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-10-02 18:17:31 +0200

    Update main.cpp
    Cleaning main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,3 @@
-// Web creator using DFS
-// Tool for creating simple network (graph) containing nodes in form of IP
-// addresses and checking nodes connections using DFS (Depth-First Search)
-// algorithm.
-
-// Input
-// Set of rows with 3 entries separated by space:
-// 1) Operation type: B - build, T - test.
-// 2) From IP.
-// 3) To IP.
-
-// Output
-// For B (build) operation display nothing. For T (test) operation "Y" if there
-// is connection between given nodes, "N" otherwise.
-
-//  Example
-//  IN
-//  B 100.100.100.1 100.100.100.2
-//  B 100.100.100.1 100.100.100.3
-//  B 100.100.100.10 100.100.100.11
-//  T 100.100.100.1 100.100.100.3
-//  T 100.100.100.10 100.100.100.2
-//  T 100.100.100.10 100.100.100.11
-//  B 100.100.100.11 100.100.100.2
-//  T 100.100.100.10 100.100.100.3
-//  T 100.100.100.100 100.100.100.103
-
-//  OUT
-//  Y
-//  N
-//  Y
-//  Y
-//  N
-
 #include <iostream>
 
 #include "WebCreator.h"
